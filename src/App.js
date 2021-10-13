8e09c5cc9d32f58dec0c163832b99ac5fc29ac87
Blessing Alfred <blealf@gmail.com>
2021-10-13 22:17:07 +0100

    Alert done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 import Alert from './components/Base/Components/Alert'
+import styled from 'styled-components'
+
+const AlertWrapper = styled.div`
+  display: block;
+  position: absolute;
+  top: 20px;
+  right: 10px;
+  overflow: hidden;
+  white-space:nowrap;
+  padding: 10px;
+`;
 
 function App() {
   return (
@@ -8,7 +18,27 @@ function App() {
       <header className="App-header">
         
       </header>
-      <Alert message="This is my sample alert" color="blue"/>
+      <AlertWrapper>
+        <Alert
+          message={(
+            <span>This is a text
+              {' '}
+              <a href="http://google.com" target="_blank" rel="noreferrer">
+                this is a link
+              </a>
+            </span>
+          )}
+          type="primary"
+          timeout={6000}
+        />
+        <Alert message="This is my sample alert" type="secondary" timeout={8000}/>
+        <Alert message="This is my sample alert" type="success" timeout={10000}/>
+        <Alert message="This is my sample alert" type="danger" timeout={12000}/>
+        <Alert message="This is my sample alert" type="alert" timeout={14000}/>
+        <Alert message="This is my sample alert" type="info" timeout={16000}/>
+        <Alert message="This is my sample alert" type="light-alert"/>
+        <Alert message="This is my sample alert" type="dark-alert" dismiss/>
+      </AlertWrapper>
     </div>
   );
 }
--- src/components/Base/Components/Alert.jsx
+++ src/components/Base/Components/Alert.jsx
@@ -1,17 +1,120 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import styled from 'styled-components'
+// import { Transition } from 'react-transition-group';
+import '../../../assets/css/base/main.scss'
 
-const AlertWrapper = styled.button`
-  padding: 5px;
-  border: 1px solid red;
-  background: blue;
-  color: rgba()
-`
-const Alert = ({ message, color}) => {
+const AlertButton = styled.button`
+  z-index: 99;
+  display: block;
+  position: relative;
+  margin-bottom: 10px;
+  right: -300px;
+  padding: 12px;
+  border: 1px solid ${props => props.textColor};
+  background: ${props => props.bgColor};
+  color: ${props => props.textColor};
+  border-radius: 3px;
+  min-width: 250px;
+  text-align: left;
+  transition: right 300ms ease-out;
+`;
+
+const Content = styled.div`
+  display: flex;
+`;
+const Dismiss = styled.div`
+  display: block;
+  margin-left: auto;
+  cursor: pointer;
+`;
+<style>
+
+</style>
+const Alert = ({ message, type, dismiss, timeout }) => {
+
+  const [textColor, setTextColor] = useState()
+  const [bgColor, setBgColor] = useState()
+  const [duration, setDuration] = useState(6000)
+
+  const alertRef = useRef()
+
+  useEffect(() => {
+    selectColor(type)
+    if (timeout) setDuration(timeout)
+    setTimeout(() => {
+      alertRef.current.style.right = '10px'
+    }, 400);
+    setTimeout(() => {
+      if (!dismiss) {
+        close()
+        setTimeout(() => {
+          alertRef.current.style.display = 'none'
+        }, 300)
+      }
+    }, duration)
+  }, [type, dismiss, duration, timeout])
+
+  const selectColor = (value) => {
+    switch (value) {
+      case 'primary':
+        setBgColor('#B9BBE5')
+        setTextColor('#393C85')
+        break
+      case 'secondary':
+        setBgColor('#C7C9D1')
+        setTextColor('#6B5486')
+        break
+      case 'success':
+        setBgColor('#AAE1CB')
+        setTextColor('#667F88')
+        break
+      case 'danger':
+        setBgColor('#FEC0BF')
+        setTextColor('#B14542')
+        break
+      case 'alert':
+        setBgColor('#FFE5BA')
+        setTextColor('#EBAF5F')
+        break
+      case 'info':
+        setBgColor('#B7DBF9')
+        setTextColor('#5B74B9')
+        break
+      case 'light-alert':
+        setBgColor('#F6F6F9')
+        setTextColor('#C0ABA7')
+        break
+      case 'dark-alert':
+        setBgColor('#AEB0B3')
+        setTextColor('#24507F')
+        break
+      default:
+        setBgColor('#93c9ff')
+        setTextColor('#2d6399')
+    }
+  }
+
+  const setDismiss = () => {
+    if(dismiss) {
+      return <Dismiss onClick={close}>X</Dismiss>
+    }
+  }
+
+  const close = () => {
+    alertRef.current.style.right =  `${-(alertRef.current.offsetWidth + 50)}px`
+  }
+
   return (
-    <AlertWrapper>
-      {message}
-    </AlertWrapper>
+    <AlertButton
+      textColor={textColor}
+      bgColor={bgColor}
+      ref={alertRef}
+    >
+      <Content>
+        {message}
+        {setDismiss()}
+      </Content>
+    </AlertButton>
   )
 }
 
